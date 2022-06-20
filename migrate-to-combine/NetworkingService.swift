2780bea27b82e4f35e9f8855ef5d64c62ce7932e
Bharghav <cbk2604@gmail.com>
2022-06-20 21:27:39 +0530

    Changed network service completion handler to Future return

--- migrate-to-combine/NetworkingService.swift
+++ migrate-to-combine/NetworkingService.swift
@@ -6,10 +6,13 @@
 //
 
 import Foundation
+import Combine
 
 enum NetworkingService {
-    static func getAnimals(completion: @escaping (Result<[Animal], Error>) -> Void) {
-        let animals: [Animal] = [.dog, .cat, .frog, .panda, .lion]
-        completion(.success(animals))
+    static func getAnimals() -> Future<[Animal], Error> {
+        Future { promise in
+            let animals: [Animal] = [.dog, .cat, .frog, .panda, .lion]
+            promise(.success(animals))
+        }
     }
 }
